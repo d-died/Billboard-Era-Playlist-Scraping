6e6224c9cd509f03b16466be9e90e5c3eb700a19
Derek Diedrich <dsumpmann@gmail.com>
2023-02-08 11:09:27 -0500

    Finished with project for now

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-load_dotenv()
 
 music_date = input("Which year do you want to travel to? Type the date in YYYY-MM-DD format: ")
 response = requests.get(f"https://www.billboard.com/charts/hot-100/{music_date}")
@@ -26,5 +25,7 @@ song_artists = [artist.getText().strip() for artist in artist_soup]
 number_one_artist = soup.find(name="span", class_="c-label a-no-trucate a-font-primary-s lrv-u-font-size-14@mobile-max u-line-height-normal@mobile-max u-letter-spacing-0021 lrv-u-display-block a-truncate-ellipsis-2line u-max-width-330 u-max-width-230@tablet-only u-font-size-20@tablet").getText().strip()
 song_artists.insert(0, number_one_artist)
 
-print(os.environ.get("CLIENT_ID"))
+
+load_dotenv()
+
 
